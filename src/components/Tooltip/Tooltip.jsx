600d47f522eb22e9111a4cba26b64fd6184d511d
SerVik888 <genom.86@inbox.ru>
2022-04-02 13:04:10 +0300

    add mousemove tooltip

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -3,10 +3,21 @@ import s from './tooltip.module.css'
 
 export const Tooltip = ({ children, text, ...rest }) => {
   const [show, setShow] = useState(false)
+  const [MousePosition, setMousePosition] = useState({
+    left: 0,
+    top: 0,
+  })
+
+  const handleMouseMove = e => {
+    setMousePosition({ left: e.pageX, top: e.pageY })
+  }
 
   return (
-    <div className={s.tooltip_container}>
-      <div className={show ? `${s.tooltip_box} ${s.visible}` : s.tooltip_box}>
+    <div className={s.tooltip_container} onMouseMove={e => handleMouseMove(e)}>
+      <div
+        className={show ? `${s.tooltip_box} ${s.visible}` : s.tooltip_box}
+        style={{ left: MousePosition.left - 10, top: MousePosition.top + 25 }}
+      >
         {text}
         <span className={s.tooltip_arrow} />
       </div>
